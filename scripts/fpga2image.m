622186f684ab557f2edc10b415ad6d14620f6bbb
Ariana Eisenstein <ajeisens@mit.edu>
2016-05-01 23:59:21 -0400

    support matlab for color images

--- scripts/fpga2image.m
+++ scripts/fpga2image.m
@@ -3,19 +3,50 @@ bytes = fread(f);
 fclose(f);
 bytes = uint8(bytes);
 L = 3;
-N = [256 256];
+N = [256 256]
+%N = [1024 1024];
 [new_coeffs,b1] = huffman_decode(bytes');
 n_c = size(new_coeffs)
-Y_n = zeros(N(1),N(2));
-for i = 1:N(1)
-    f = (i-1)*N(1)+1;
-    s = (i)*N(1);
-    s = min(n_c(2),s);
-    Y_n(i,1:s+1-f) = new_coeffs(f:s);
+Y_R_n = zeros(N(1),N(2));
+Y_G_n = zeros(N(1),N(2));
+Y_B_n = zeros(N(1),N(2));
+O = zeros(N(1),N(2),3);
+for c = 1:3
+    for i = 1:N(1)
+        f = (i-1)*N(1)+1+(N(1)*(c-1))^2;
+        s = (i)*N(1)+(N(1)*(c-1))^2;
+        if (s < N(1)*N(1)*3)
+            switch c
+                case 1
+                    Y_R_n(i,:) = new_coeffs(f:s);
+                case 2
+                    Y_G_n(i,:) = new_coeffs(f:s);
+                case 3
+                    Y_B_n(i,:) = new_coeffs(f:s);
+            end
+        end
+    end
 end
 for j=1:L
-    Y_n=unscramble(Y_n, L-j);
+    Y_R_n=unscramble(Y_R_n, L-j);
+    Y_G_n=unscramble(Y_G_n, L-j);
+    Y_B_n=unscramble(Y_B_n, L-j);
 end
-R = waveletcdf97(Y_n,-L);
-R = R + 128;
-imshow(uint8(R));
+O_R = (waveletcdf97(Y_R_n,-L)+128)/255;
+O_G = (waveletcdf97(Y_G_n,-L)+128)/255;
+O_B = (waveletcdf97(Y_B_n,-L)+128)/255;
+for i = 1:N(1)
+    for j = 1:N(2)
+        for c = 1:3
+            switch c
+                case 1
+                    O(i,j,c) = O_R(i,j);
+                case 2
+                    O(i,j,c) = O_G(i,j);
+                case 3
+                    O(i,j,c) = O_B(i,j);
+            end
+        end
+    end
+end
+imshow(O);
--- scripts/image2fpga.m
+++ scripts/image2fpga.m
@@ -1,28 +1,62 @@
-ImageFile = 'saturn.png';
-t =imread(ImageFile);
-X = double(rgb2gray(imread(ImageFile)));
-ns = [256, 256];
-[I,J]=meshgrid(linspace(1,size(X,2),ns(1)),linspace(1,size(X,1),ns(2)));
-X = interp2(X, I, J);%crop;padarray(X,[256-N(1),256-N(2),0],'post');
-imshow(uint8(X));
+S = 256;
+% ImageFile = 'saturn_1024.jpg';
+% t =imread(ImageFile);
+% X = double(imread(ImageFile));
+% N = size(X);
+% X = padarray(X,[1024-N(1),1024-N(2),0],'post')-128;
+% N = size(X)
+% L = 3;
 
-X = X - 128;
-%X = rgb2ycbcr(X); 
-%X = X(:,:,1);
-%X = randi([-4,3],8,8);
-%X = ones(8);
-N = size(X)
+ImageFile = 'toysnoflash_256.png';
+%t =imread(ImageFile);
+X = imread(ImageFile);
+% X = rgb2gray(X);
+% N = size(X);
+% X = padarray(X,[S-N(1),S-N(2),0],'post')-128;
+% X = double(X);
+% N = size(X);
+N = size(X);
+R = padarray(X(:,:,1),[S-N(1),S-N(2),0],'post')-128;
+G = padarray(X(:,:,2),[S-N(1),S-N(2),0],'post')-128;
+B = padarray(X(:,:,3),[S-N(1),S-N(2),0],'post')-128;
+
+R = double(R);
+G = double(G);
+B = double(B);
+N = size(R);
 L = 3;
-Y = waveletcdf97(X,L);
+Y_R = waveletcdf97(R,L);
+Y_G = waveletcdf97(G,L);
+Y_B = waveletcdf97(B,L);
 for i=1:L
-    Y=scramble(Y, i-1);
+    Y_R=scramble(Y_R, i-1);
+    Y_G=scramble(Y_G, i-1);
+    Y_B=scramble(Y_B, i-1);
 end
+% Y = waveletcdf97(X,L)Y = waveletcdf97(X,L);
+% for i=1:L
+%     Y=scramble(Y, i-1);
+% end
 T =[];
-for i = 1:N(1)
-    f = (i-1)*N(1)+1;
-    s = (i)*N(1);
-    T(f:s) = Y(i,:);
+for c = 1:3
+    for i = 1:N(1)
+        f = (i-1)*N(1)+1+(N(1)*(c-1))^2;
+        s = (i)*N(1)+(N(1)*(c-1))^2;
+        switch c
+            case 1  
+                T(f:s) = Y_R(i,:);
+            case 2
+                T(f:s) = Y_G(i,:);
+            case 3
+                T(f:s) = Y_B(i,:);
+        end
+    end
 end
+% for i =1:N(1)
+%     f = (i-1)*N(1)+1;
+%     s = (i)*N(1);
+%     T(f:s) = Y(i,:);
+% end
 T = int16(T);
 size(T)
 coeffs = [0,3,-4,2,-1,-1,0,47,55,1,-1,2,-2,3,-3,-4,0,0,0,0,0,0,0];
